342c0b2266530b53d775e529cbada2833c80c45f
Matt Moravec <moravemj@Matts-MacBook-Pro.local>
2016-11-09 14:58:32 -0800

    NavigationProvider setup correctly

--- main.js
+++ main.js
@@ -1,28 +1,37 @@
 import Exponent from 'exponent';
 import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import { NavigationProvider, StackNavigation, createRouter } from '@exponent/ex-navigation';
+import HomeScreen from './screens/Homescreen.js';
+/**
+  * This is where we map route names to route components. Any React
+  * component can be a route, it only needs to have a static `route`
+  * property defined on it, as in HomeScreen below
+  */
+const Router = createRouter(() => ({
+  home: () => HomeScreen,
+}));
 
 class App extends React.Component {
   render() {
+    /**
+      * NavigationProvider is only needed at the top level of the app,
+      * similar to react-redux's Provider component. It passes down
+      * navigation objects and functions through context to children.
+      *
+      * StackNavigation represents a single stack of screens, you can
+      * think of a stack like a stack of playing cards, and each time
+      * you add a screen it slides in on top. Stacks can contain
+      * other stacks, for example if you have a tab bar, each of the
+      * tabs has its own individual stack. This is where the playing
+      * card analogy falls apart, but it's still useful when thinking
+      * of individual stacks.
+      */
     return (
-      <View style={styles.container}>
-        <Text>Open up main.js to start working on your app!</Text>
-      </View>
+      <NavigationProvider router={Router}>
+        <StackNavigation initialRoute={Router.getRoute('home')} />
+      </NavigationProvider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 Exponent.registerRootComponent(App);
