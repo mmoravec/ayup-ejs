821d4c1fdda7c3cb64c0f11ed54a0c0043524a5a
Matt Moravec <moravemj@Matts-MacBook-Pro.local>
2016-12-01 15:24:29 -0800

    mirrored brent's project

--- main.js
+++ main.js
@@ -1,20 +1,97 @@
 import Exponent from 'exponent';
 import React from 'react';
-import { NavigationProvider } from '@exponent/ex-navigation';
-import App from './app';
+import { StyleSheet, View } from 'react-native';
+import { NavigationProvider, withNavigation, StackNavigation } from '@exponent/ex-navigation';
+import { Provider as ReduxProvider, connect } from 'react-redux';
+
+import Actions from './state/Actions';
 import Router from './navigation/Router';
-//import { Provider as ReduxProvider, connect } from 'react-redux'
+import Store from './state/Store';
+import { User } from './state/Records';
+import LocalStorage from './state/LocalStorage';
 
 class AppContainer extends React.Component {
   render() {
     return (
-//      <ReduxProvider store={Store}>
+     <ReduxProvider store={Store}>
         <NavigationProvider router={Router}>
-          <App />
+          <App {...this.props} />
         </NavigationProvider>
-//      </ReduxProvider>
+     </ReduxProvider>
     );
   }
 }
+
+
+@withNavigation
+@connect(data => App.getDataProps)
+class App extends React.Component {
+  static getDataProps(data) {
+    return {
+      currentUser: data.currentUser,
+    };
+  }
+
+  state = {
+    dataReady: false,
+  };
+
+  async componentDidMount() {
+    await this._loadCacheAsync();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (!this.state.dataReady) {
+      return;
+    }
+
+    const rootNavigator = this.props.navigation.getNavigator('root');
+    const previouslySignedIn = isSignedIn(prevProps.currentUser) &&
+      prevState.dataReady === this.state.dataReady;
+    const currentlySignedIn = isSignedIn(this.props.currentUser);
+
+    if (!previouslySignedIn && currentlySignedIn) {
+      rootNavigator.replace('home');
+    } else if (previouslySignedIn && !currentlySignedIn) {
+      rootNavigator.replace('home');
+    }
+  }
+
+
+  _loadCacheAsync = async () => {
+    let user = new User(await LocalStorage.getUserAsync());
+    this.props.dispatch(Actions.setCurrentUser(user));
+
+    this.setState({
+      dataReady: true,
+    });
+  }
+
+  render() {
+    if (!this.state.dataReady) {
+      return <Exponent.Components.AppLoading />;
+    }
+
+    return (
+      <View style={styles.container}>
+        <StackNavigation
+          id="root"
+          initialRoute={Router.getRoute('home')}
+        />
+      </View>
+    );
+  }
+}
+
+
+function isSignedIn(userState) {
+  return !!userState.authToken || userState.isGuest;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 Exponent.registerRootComponent(AppContainer);
--- screens/Homescreen.js
+++ screens/Homescreen.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import {
-  View,
-  Text
-} from 'react-native';
+import { Components } from 'exponent';
 
 export default class HomeScreen extends React.Component {
 
   render() {
     return (
-      <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
-        <Text>HomeScreen!</Text>
-      </View>
+      <Components.MapView
+        style={{ flex: 1, backgroundColor: '#fff' }}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <Components.MapView.UrlTile
+          urlTemplate="http://a.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png"
+        />  
+      </Components.MapView>
     );
   }
 }
